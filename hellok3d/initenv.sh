b92f0703fa1a8726fa72e8e38848f895f5832852
alessandroargentieri <alexmawashi87@gmail.com>
2020-12-05 14:45:01 +0100

    afwd

--- hellok3d/initenv.sh
+++ hellok3d/initenv.sh
@@ -6,3 +6,10 @@ snap install figlet
 echo "alias clusterinit='k3d cluster create'" >> .bashrc
 echo "alias login='figlet Codemotion Cloud'" >> .bashrc
 figlet "Ciao"
+
+
+echo "       _                 _         "
+echo " _   _| |__  _   _ _ __ | |_ _   _ "
+echo "| | | | '_ \| | | | '_ \| __| | | |"
+echo "| |_| | |_) | |_| | | | | |_| |_| |"
+echo " \__,_|_.__/ \__,_|_| |_|\__|\__,_|"
