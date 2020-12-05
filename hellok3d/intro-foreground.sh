92605f54f0e6f448eea016e6ad0d0db1f4b18f68
alessandroargentieri <alexmawashi87@gmail.com>
2020-12-05 10:15:48 +0100

    intro foreground

---
+++ hellok3d/intro-foreground.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+echo "Welcome to"
+figlet "Codemotion Cloud"
+echo "###########"
+
+echo "Check your existing kubernetes cluster with: "
+echo "kubectl cluster-info"
+echo " "
+echo "or create a new cluster with: "
+echo "clusterinit demo --servers 1 --agents 2"
+
