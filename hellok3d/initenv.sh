717873336f733db43e8cad5dddce2545ad6d3cf3
alessandroargentieri <alexmawashi87@gmail.com>
2020-12-05 10:06:12 +0100

    aad

--- hellok3d/initenv.sh
+++ hellok3d/initenv.sh
@@ -3,5 +3,5 @@
 curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
 snap install kubectl --classic
 snap install figlet
-alias clusterinit='k3d cluster create' >> ~/.bashrc
-echo 'figlet "Codemotion Cloud"' >> ~/.bashrc
+alias clusterinit='k3d cluster create' >> .bashrc
+echo 'figlet "Codemotion Cloud"' >> .bashrc
